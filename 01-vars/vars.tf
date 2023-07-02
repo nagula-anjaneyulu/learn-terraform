d91a98ac6a372538118a879534762f3a17893310
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-02 17:57:10 +0530

    roboshop-terraform

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -2,16 +2,3 @@ variable "sample_string" {
     default = "Hello World"
 }
 ## shell scripting equivalent sample_string="Hello world"
-#To print in shell script we use echo,following is the syntax in shell
-# echo $ sample_string
-
-output "sample_string" {
-    value=var.sample_string
-}
-
-##In above case we are printing just variable, If variable is a combination of some
-#strings then following is the syntax,${}is mandatory if we include the variable inside a string
-
-output "sample_string1" {
-        value="value of sample_string=${var.sample_string}"
-}
