5dff59a757f116a9895680987c1674883c04e8fd
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-02 18:20:30 +0530

    learn-terraform

--- 01-vars/outputs.tf
+++ 01-vars/outputs.tf
@@ -11,3 +11,10 @@ output "sample_string" {
 output "sample_string1" {
   value="value of sample_string=${var.sample_string}"
 }
+##values from list
+#second value in list
+
+output "sample_list_2" {
+  value = "var.sample_list[1]"
+}
+
