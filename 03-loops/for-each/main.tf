7943c56ca20001e18abd937e6e4f108ee6ada00a
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-11 00:51:15 +0530

    \\

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name- $ {var.fruits[count.index]}"
+  }
+}
+
+variable "fruits" {
+  default = {
+    apple=10
+    banana=200
+    orange=100
+  }
+  }
