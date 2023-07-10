01bea3ef99d1b892cf9794d830fc8ce002dd9286
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-10 19:23:22 +0530

    \\

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,3 +1,10 @@
 resource "null_resource" "null" {
   count = 20
 }
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+}
+
+variable "fruits" {
+  default = ["apple","banana","orange"]
+}
