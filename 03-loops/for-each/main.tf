01d63599f6a182f83b0d572a4cf90651b48a5312
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-11 17:05:43 +0530

    \\

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,8 +1,9 @@
 resource "null_resource" "fruits" {
+
   for_each = var.fruits
 
   provisioner "local-exec" {
-    command = "echo Fruit Name- $ {var.fruits[each.key]} - {var.fruits[each.value}"
+    command = "echo Fruit Name- $ {var.fruits[each.key]}"
     //command = "echo ${length(var.fruits)}"
   }
 }
