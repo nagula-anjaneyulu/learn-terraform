ea9011767ff8978bf377c469fb452a5d5d18e163
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-14 17:19:31 +0530

    terraform

--- 04-Functions/main.tf
+++ 04-Functions/main.tf
@@ -1,7 +1,23 @@
 variable "sample" {
   default = "abcxyz"
 }
+//output "sample" {
+//value = var.sample
+//}
 
+# convert lower case to uppercase
 output "sample" {
   value = upper(var.sample)
 }
+
+variable "sample1" {
+  default = {
+    abc = 100
+    xyz = 200
+  }
+}
+
+output "sample2" {
+  value = element(var.sample1,0 )
+}
+
