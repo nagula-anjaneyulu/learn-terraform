d74ff3ed2d869b75ff0986f3609fad6e361450e1
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-18 16:36:35 +0530

    terraform

--- 06-module/module/main.tf
+++ 06-module/module/main.tf
@@ -4,13 +4,13 @@ resource "null_resource" "nothing" {
   }
 }
 
-resource "null_resource" "nothing" {
+resource "null_resource" "nothing1" {
   provisioner "local-exec" {
     command = "echo hello from module"
   }
 }
 
-resource "null_resource" "nothing" {
+resource "null_resource" "nothing2" {
   provisioner "local-exec" {
     command = "echo hello from module"
   }
