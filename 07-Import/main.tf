0183e8b2e18694beee6284a5320f8ab18fc45f70
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-20 16:53:20 +0530

    terraform

--- 07-Import/main.tf
+++ 07-Import/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "workstation" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.small"
-  tags                                 = {
+  tags = {
      "Name" = "workstation"
   }
 
